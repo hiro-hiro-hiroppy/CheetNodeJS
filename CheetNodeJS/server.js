abaed3a38814ea757142deb1839b929f21ad35cf
hirohirohiroppy <mokopokox@gmail.com>
2021-05-16 12:54:10 +0900

    一旦追加

--- CheetNodeJS/server.js
+++ CheetNodeJS/server.js
@@ -1,9 +1,48 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http = require("http");
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//import http = require('http');
+//var port = process.env.port || 1337
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+//------------------------------
+//  �T�[�o�[�N������
+//------------------------------
+const express = require('express');
+const app = express();
+const port = process.env.port || 1337;
+const path = require('path');
+let server = app.listen(port, function () {
+    console.log('Node.js Start');
+});
+//------------------------------
+// API
+//------------------------------
+let html_or_api = true;
+//req��http���N�G�X�g��\���I�u�W�F�N�g
+//res��http���X�|���X���\�����邽�߂̃I�u�W�F�N�g
+//next�́�https://qiita.com/syumiwohossu/items/f9ee317f31adc3ad387b
+//HTML
+//app.get('/', function (req, res) {
+//    res.sendFile(__dirname + '/index.html');
+//});
+// �ÓI�t�@�C���̃��[�e�B���O
+app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, 'Page')));
+//WebAPI
+app.get('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+app.put('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+app.put('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+app.delete('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+// ���̑��̃��N�G�X�g�ɑ΂���404�G���[
+app.use((req, res) => {
+    res.sendStatus(404);
+});
 //# sourceMappingURL=server.js.map
--- CheetNodeJS/server.ts
+++ CheetNodeJS/server.ts
@@ -1,6 +1,55 @@
-import http = require('http');
-var port = process.env.port || 1337
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//import http = require('http');
+//var port = process.env.port || 1337
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+//------------------------------
+//  サーバー起動処理
+//------------------------------
+const express = require('express');
+const app = express();
+const port = process.env.port || 1337;
+const path = require('path');
+
+let server = app.listen(port, function () {
+
+    console.log('Node.js Start');
+})
+
+//------------------------------
+// API
+//------------------------------
+
+//reqはhttpリクエストを表すオブジェクト
+//resはhttpレスポンスを構成するためのオブジェクト
+//nextは→https://qiita.com/syumiwohossu/items/f9ee317f31adc3ad387b
+
+//HTML
+// 静的ファイルのルーティング
+app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, 'Page')));
+
+
+//WebAPI
+app.get('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.put('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.put('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.delete('/api/', function (req, res) {
+    res.send('Hello World!');
+});
+
+// その他のリクエストに対する404エラー
+app.use((req, res) => {
+    res.sendStatus(404);
+});
